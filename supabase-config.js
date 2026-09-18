0295617b9aea346b7033aab77f0a33a36f26c6ec
Ivan Voitekhovych <0binirovoni0@gmail.com>
2026-09-18 22:17:06 +0200

    Update Supabase URL in configuration

--- supabase-config.js
+++ supabase-config.js
@@ -12,5 +12,5 @@
    guide). Never put the service_role key in front-end code.
    ====================================================================== */
 
-const SUPABASE_URL = 'https://xkjyittjewiqjtcbwxtm.supabase.co/rest/v1/';
+const SUPABASE_URL = 'https://xkjyittjewiqjtcbwxtm.supabase.co';
 const SUPABASE_ANON_KEY = 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6InhranlpdHRqZXdpcWp0Y2J3eHRtIiwicm9sZSI6ImFub24iLCJpYXQiOjE3ODk3NTU3ODIsImV4cCI6MjEwNTMzMTc4Mn0.r9ZiZSWYphwk8tYDTiZ4eO2Wcet9m9hDNF4X0nSCuRY';
